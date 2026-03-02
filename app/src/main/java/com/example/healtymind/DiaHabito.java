a6cef0436d93a0eae97d94689010d4bec8f8ef92
Cristopher Armando Flores Bahena <cristopher_13240109235@uptecamac.edu.mx>
2026-03-02 02:22:55 -0600

    Funcionamiento del calendario para la racha de la aplicacion Healtymind

---
+++ app/src/main/java/com/example/healtymind/DiaHabito.java
@@ -0,0 +1,11 @@
+package com.example.healtymind;
+
+public class DiaHabito {
+    int num_dia;
+    boolean dia_hecho;
+
+    public DiaHabito(int num_dia, boolean dia_hecho){
+        this.num_dia = num_dia;
+        this.dia_hecho = dia_hecho;
+    }
+}
